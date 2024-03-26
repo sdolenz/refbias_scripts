ef2e3e2c1de6e584ae0105aa8580d40dc05190e1
sdolenz <114164155+sdolenz@users.noreply.github.com>
2024-03-26 10:32:57 +0100

    Update simulated_pipeline.sh

--- simulated_pipeline.sh
+++ simulated_pipeline.sh
@@ -1,6 +1,12 @@
 #Here is the pipeline that will submit the scripts to simulated diverged reads, build simulated libraries, and map the simulated libraries to the appropriate reference genome using various alignment tools
 
-#Dependencies: 
+#Dependencies: python/3.9.5
+
+
+#download gargammel from github, this will create a directory "gargammel" within your parent directory
+
+path=...your parent directory.../gargammel
+cd ${path}
 
 # Create mismatch files to simulate 1, 2, 3, 4, 5, 6, 9, 12, and 15 divergence
 
@@ -10,3 +16,12 @@ bash simulated_mismatch.sh
 
 bash simulated_simulations.sh
 
+# Create and change to sim_libraries directory 
+
+path2=...your parent directory.../sim_libraries
+mkdir -p ${path2}
+cd ${path2}
+
+# Construct simulated libraries 
+
+bash simulated_building_libraries
